e768689d177dee112ea0da9baa093d673608abca
Ningxin Hu <ningxin.hu@intel.com>
2020-02-03 18:49:05 +0800

    Add fp16 support
    squeezenet.exe fp16 vpu

--- samples/c_cxx/squeezenet/main.cc
+++ samples/c_cxx/squeezenet/main.cc
@@ -48,6 +48,73 @@ class Timer {
   double m_startTime;
 };
 
+class Float16Compressor {
+  union Bits {
+    float f;
+    int32_t si;
+    uint32_t ui;
+  };
+
+  static int const shift = 13;
+  static int const shiftSign = 16;
+
+  static int32_t const infN = 0x7F800000;   // flt32 infinity
+  static int32_t const maxN = 0x477FE000;   // max flt16 normal as a flt32
+  static int32_t const minN = 0x38800000;   // min flt16 normal as a flt32
+  static int32_t const signN = 0x80000000;  // flt32 sign bit
+
+  static int32_t const infC = infN >> shift;
+  static int32_t const nanN = (infC + 1) << shift;  // minimum flt16 nan as a flt32
+  static int32_t const maxC = maxN >> shift;
+  static int32_t const minC = minN >> shift;
+  static int32_t const signC = signN >> shiftSign;  // flt16 sign bit
+
+  static int32_t const mulN = 0x52000000;  // (1 << 23) / minN
+  static int32_t const mulC = 0x33800000;  // minN / (1 << (23 - shift))
+
+  static int32_t const subC = 0x003FF;  // max flt32 subnormal down shifted
+  static int32_t const norC = 0x00400;  // min flt32 normal down shifted
+
+  static int32_t const maxD = infC - maxC - 1;
+  static int32_t const minD = minC - subC - 1;
+
+ public:
+  static uint16_t compress(float value) {
+    Bits v, s;
+    v.f = value;
+    uint32_t sign = v.si & signN;
+    v.si ^= sign;
+    sign >>= shiftSign;  // logical shift
+    s.si = mulN;
+    s.si = static_cast<int32_t>(s.f * v.f);  // correct subnormals
+    v.si ^= (s.si ^ v.si) & -(minN > v.si);
+    v.si ^= (infN ^ v.si) & -((infN > v.si) & (v.si > maxN));
+    v.si ^= (nanN ^ v.si) & -((nanN > v.si) & (v.si > infN));
+    v.ui >>= shift;  // logical shift
+    v.si ^= ((v.si - maxD) ^ v.si) & -(v.si > maxC);
+    v.si ^= ((v.si - minD) ^ v.si) & -(v.si > subC);
+    return static_cast<uint16_t>(v.ui | sign);
+  }
+
+  static float decompress(uint16_t value) {
+    Bits v;
+    v.ui = value;
+    int32_t sign = v.si & signC;
+    v.si ^= sign;
+    sign <<= shiftSign;
+    v.si ^= ((v.si + minD) ^ v.si) & -(v.si > subC);
+    v.si ^= ((v.si + maxD) ^ v.si) & -(v.si > maxC);
+    Bits s;
+    s.si = mulC;
+    s.f *= v.si;
+    int32_t mask = -(norC > v.si);
+    v.si <<= shift;
+    v.si ^= (s.si ^ v.si) & mask;
+    v.si |= sign;
+    return v.f;
+  }
+};
+
 using winrt::com_ptr;
 using winrt::check_hresult;
 using winrt::check_bool;
@@ -169,6 +236,7 @@ void InitWithDXCore(com_ptr<ID3D12Device>& d3D12Device,
       commandList.put_void()));
 }
 
+static void usage() { printf("usage: [fp32|fp16] [cpu|gpu|vpu] \n"); }
 
 int main(int argc, char* argv[]) {
 #if USE_DML
@@ -177,8 +245,20 @@ int main(int argc, char* argv[]) {
   com_ptr<ID3D12CommandAllocator> commandAllocator;
   com_ptr<ID3D12GraphicsCommandList> commandList;
 #endif
-  // ep could be "dml" or default (cpu)
-  char* ep = (argc >= 2) ? argv[1] : "";
+  if (argc < 3) {
+    usage();
+    return -1;
+  }
+  char* type = argv[1];
+  BOOL request_fp16;
+  if (std::string(type) == "fp32") {
+    request_fp16 = FALSE;
+  } else if (std::string(type) == "fp16") {
+    request_fp16 = TRUE;
+  } else {
+    usage();
+    return -1;
+  }
 
   //*************************************************************************
   // initialize  enviroment...one enviroment per process
@@ -193,11 +273,10 @@ int main(int argc, char* argv[]) {
   // session (we also need to include cuda_provider_factory.h above which defines it)
   // #include "cuda_provider_factory.h"
   // OrtSessionOptionsAppendExecutionProvider_CUDA(session_options, 1);
-
-  if (std::string(ep) == std::string("dml")) {
+  char* device = argv[2];
+  if (std::string(device) == std::string("gpu") ||
+      std::string(device) == std::string("vpu")) {
     std::wcout << "Using DML" << std::endl;
-	// device could be "vpu" or default (gpu).
-    char* device = (argc >= 3) ? argv[2] : "";
     bool use_vpu = std::string(device) == "vpu";
 #ifdef USE_DML
     // Set up Direct3D 12.
@@ -212,14 +291,20 @@ int main(int argc, char* argv[]) {
         __uuidof(dmlDevice),
         dmlDevice.put_void()));
 
-    DML_FEATURE_DATA_TENSOR_DATA_TYPE_SUPPORT support_f16 = {false};
+    DML_FEATURE_DATA_TENSOR_DATA_TYPE_SUPPORT support_fp16 = {false};
     DML_FEATURE_QUERY_TENSOR_DATA_TYPE_SUPPORT fp16_query = {
         DML_TENSOR_DATA_TYPE_FLOAT16};
     check_hresult(dmlDevice->CheckFeatureSupport(
         DML_FEATURE_TENSOR_DATA_TYPE_SUPPORT, sizeof(fp16_query), &fp16_query,
-        sizeof(support_f16), &support_f16));
+        sizeof(support_fp16), &support_fp16));
 
-    std::wcout << "Support float16: " << support_f16.IsSupported << std::endl;
+    std::wcout << "Support float16: " << support_fp16.IsSupported << std::endl;
+    std::wcout << "Request float16: " << request_fp16 << std::endl;
+
+	if (request_fp16 && !support_fp16.IsSupported) {
+      std::wcout << "Device doesn't support float16" << std::endl;
+      return -1;
+    }
 
     session_options.DisableMemPattern();
     session_options.SetExecutionMode(ORT_SEQUENTIAL);
@@ -228,8 +313,11 @@ int main(int argc, char* argv[]) {
     std::wcout << "DML is not enabled in this build" << std::endl;
     return -1;
 #endif
-  } else {
+  } else if (std::string(device) == std::string("cpu")) {
     std::wcout << "Using CPU" << std::endl;
+  } else {
+    usage();
+    return -1;
   }
 
   // Sets graph optimization level
@@ -245,10 +333,16 @@ int main(int argc, char* argv[]) {
   // using squeezenet version 1.3
   // URL = https://github.com/onnx/models/tree/master/squeezenet
 #ifdef _WIN32
-  const wchar_t* model_path = L"SqueezeNet.onnx";
+  const wchar_t* model_path;
+  if (request_fp16) {
+    model_path = L"SqueezeNet_fp16.onnx";
+  } else {
+    model_path = L"SqueezeNet.onnx";
+  }
 #else
   const char* model_path = "squeezenet.onnx";
 #endif
+  std::wcout << L"Using model: " << std::wstring(model_path) << std::endl;
 
   printf("Using Onnxruntime C++ API\n");
   Ort::Session session(env, model_path, session_options);
@@ -259,7 +353,8 @@ int main(int argc, char* argv[]) {
 
   // print number of model input nodes
   size_t num_input_nodes = session.GetInputCount();
-  std::vector<const char*> input_node_names;
+  const char* input_node_name;
+  size_t input_node_index;
   std::vector<int64_t> input_node_dims;  // simplify... this model has only 1 input node {1, 3, 224, 224}.
                                          // Otherwise need vector<vector<>>
 
@@ -272,7 +367,8 @@ int main(int argc, char* argv[]) {
     if (std::string(input_name) != std::string("data_0"))
       continue;
     printf("Input %d : name=%s\n", i, input_name);
-    input_node_names.push_back(input_name);
+    input_node_name = input_name;
+    input_node_index = i;
 
     // print input node types
     Ort::TypeInfo type_info = session.GetInputTypeInfo(i);
@@ -286,6 +382,7 @@ int main(int argc, char* argv[]) {
     printf("Input %d : num_dims=%zu\n", i, input_node_dims.size());
     for (int j = 0; j < input_node_dims.size(); j++)
       printf("Input %d : dim %d=%jd\n", i, j, input_node_dims[j]);
+    break;
   }
 
   // Results should be...
@@ -310,15 +407,26 @@ int main(int argc, char* argv[]) {
                                              // use OrtGetTensorShapeElementCount() to get official size!
 
   std::vector<float> input_tensor_values(input_tensor_size);
+  std::vector<uint16_t> fp16_input_tensor_values(input_tensor_size);
+  std::vector<const char*> input_node_names = {input_node_name};
   std::vector<const char*> output_node_names = {"softmaxout_1"};
 
   // initialize input data with values in [0.0, 1.0]
-  for (unsigned int i = 0; i < input_tensor_size; i++)
-    input_tensor_values[i] = (float)i / (input_tensor_size + 1);
-
+  for (unsigned int i = 0; i < input_tensor_size; i++) {
+    if (request_fp16) {
+      fp16_input_tensor_values[i] = Float16Compressor::compress((float)i / (input_tensor_size + 1));
+    } else {
+      input_tensor_values[i] = (float)i / (input_tensor_size + 1);
+    }
+  }
   // create input tensor object from data values
   auto memory_info = Ort::MemoryInfo::CreateCpu(OrtArenaAllocator, OrtMemTypeDefault);
-  Ort::Value input_tensor = Ort::Value::CreateTensor<float>(memory_info, input_tensor_values.data(), input_tensor_size, input_node_dims.data(), 4);
+  Ort::Value input_tensor = Ort::Value::CreateTensor(
+	  memory_info, 
+	  request_fp16 ? (void*)fp16_input_tensor_values.data() : (void*)input_tensor_values.data(), 
+	  request_fp16 ? input_tensor_size * sizeof(uint16_t) : input_tensor_size * sizeof(float),
+	  input_node_dims.data(), 4, 
+	  request_fp16 ? ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT16 : ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT);
   assert(input_tensor.IsTensor());
 
   std::vector<Ort::Value> output_tensors;
@@ -331,20 +439,27 @@ int main(int argc, char* argv[]) {
     std::wcout << L"Predict " << i << " elapsed time: " << timer.Stop() << " ms\n";
   }
 
-  // Get pointer to output tensor float values
-  float* floatarr = output_tensors.front().GetTensorMutableData<float>();
-  assert(abs(floatarr[0] - 0.000045) < 1e-6);
+  if (request_fp16) {
+    uint16_t* uint16arr = output_tensors.front().GetTensorMutableData<uint16_t>();
+    assert(abs(Float16Compressor::decompress(uint16arr[0]) - 0.000045) < 5.0f * 0.0009765625);
+    for (int i = 0; i < 5; i++)
+      printf("Score for class [%d] =  %f\n", i, Float16Compressor::decompress(uint16arr[i]));
+  } else {
+    // Get pointer to output tensor float values
+    float* floatarr = output_tensors.front().GetTensorMutableData<float>();
+    assert(abs(floatarr[0] - 0.000045) < 1e-6);
 
-  // score the model, and print scores for first 5 classes
-  for (int i = 0; i < 5; i++)
-    printf("Score for class [%d] =  %f\n", i, floatarr[i]);
+    // score the model, and print scores for first 5 classes
+    for (int i = 0; i < 5; i++)
+      printf("Score for class [%d] =  %f\n", i, floatarr[i]);
 
-  // Results should be as below...
-  // Score for class[0] = 0.000045
-  // Score for class[1] = 0.003846
-  // Score for class[2] = 0.000125
-  // Score for class[3] = 0.001180
-  // Score for class[4] = 0.001317
+    // Results should be as below...
+    // Score for class[0] = 0.000045
+    // Score for class[1] = 0.003846
+    // Score for class[2] = 0.000125
+    // Score for class[3] = 0.001180
+    // Score for class[4] = 0.001317
+  }
   printf("Done!\n");
   return 0;
 }
